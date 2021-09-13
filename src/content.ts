c22862cc4b4d0c4d7c3b7c7488c75b9bab2a6bf4
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-13 15:11:18 +0100

    auto click raven

--- src/content.ts
+++ src/content.ts
@@ -1,2 +1,12 @@
 // This file is injected as a content script
-console.log("Hello from content script!")
+setTimeout(() => {
+  const loginByRavenButton: HTMLElement = document.getElementsByClassName(
+    "btn btn-secondary btn-lg btn-block mt-3"
+  )[0] as HTMLElement;
+  loginByRavenButton.click();
+
+  const ravenLogInButton: HTMLElement = document.getElementsByClassName(
+    "campl-btn"
+  )[0] as HTMLElement;
+  ravenLogInButton.click();
+}, 500);
