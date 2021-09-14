5caab96d2205cea029c574a0826549bd1c0d29b3
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-14 22:07:27 +0100

    background detects set password

--- src/background.ts
+++ src/background.ts
@@ -4,13 +4,15 @@ import { MessageTypes } from "./types";
 console.log("Hello from background script!");
 
 chrome.runtime.onMessage.addListener((message: MessageTypes) => {
-  //  chrome.cookies.getAll({}, (cks) => {
-  //   console.log("cookies", cks);
-  // });
   console.log(message);
   switch (message.type) {
     case "SetPrevPage":
       chrome.storage.local.set({ prevURL: message.url });
+      break;
+    case "SetEncrpytedPassword":
+      chrome.storage.local.set({
+        encryptedPassword: message.encryptedPassword,
+      });
       break;
     default:
       break;
