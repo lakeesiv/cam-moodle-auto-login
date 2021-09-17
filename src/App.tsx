6c1a7010734d76455b616fbe74850199832fbc4f
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-17 19:22:13 +0100

    style improvements

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ const App = () => {
   const [password, setPassword] = useState<string>("");
   return (
     <div className="App">
-      <Box>
+      <Box w="xl" h="xl" bgColor="gray.900" p={4}>
         <Input
+          p={2}
+          bgColor="gray.800"
           type="password"
           onChange={(e) => setPassword(e.target.value)}
         ></Input>
-        <Button onClick={() => sendEncryptedPassword(password)}></Button>
+        <Button onClick={() => sendEncryptedPassword(password)}>
+          Set Password
+        </Button>
       </Box>
     </div>
   );
