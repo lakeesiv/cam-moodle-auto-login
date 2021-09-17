e3eae6970fa3c77f477c44534f2c9415d495f9d4
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-17 19:16:33 +0100

    inital theme

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  extendTheme,
+  ThemeConfig,
+  ColorModeScript,
+} from "@chakra-ui/react";
 
 import App from "./App";
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+const theme = extendTheme({ config });
 
 var mountNode = document.getElementById("popup");
 ReactDOM.render(
   <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>,
   mountNode
