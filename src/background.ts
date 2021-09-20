7d874a8b3687a76e75c59dec0ad9bab890a1b42c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-20 18:18:29 +0100

    send response

--- src/background.ts
+++ src/background.ts
@@ -8,27 +8,30 @@ import { sendMessageFromBackground } from "./utils";
 // This file is ran as a background script
 console.log("Hello from background script!");
 
-chrome.runtime.onMessage.addListener((message: MessageTypes) => {
-  console.log(message);
-  switch (message.type) {
-    case "SetPrevPage":
-      chrome.storage.local.set({ prevURL: message.url });
-      break;
-    case "SetEncrpytedPassword":
-      chrome.storage.local.set({
-        encryptedPassword: message.encryptedPassword,
-      } as EncryptedPasswordObject);
-      break;
-    case "GetEncryptedPassword":
-      chrome.storage.local.get(["encryptedPassword"], (res) => {
-        const message: ReturnEncryptedPassword = {
-          type: "ReturnEncryptedPassword",
-          encryptedPassword: (res as EncryptedPasswordObject).encryptedPassword,
-        };
-        sendMessageFromBackground(message);
-      });
-      break;
-    default:
-      break;
+chrome.runtime.onMessage.addListener(
+  (message: MessageTypes, _, sendResponse) => {
+    console.log(message);
+    switch (message.type) {
+      case "SetPrevPage":
+        chrome.storage.local.set({ prevURL: message.url });
+        break;
+      case "SetEncrpytedPassword":
+        chrome.storage.local.set({
+          encryptedPassword: message.encryptedPassword,
+        } as EncryptedPasswordObject);
+        break;
+      case "GetEncryptedPassword":
+        chrome.storage.local.get(["encryptedPassword"], (res) => {
+          const message: ReturnEncryptedPassword = {
+            type: "ReturnEncryptedPassword",
+            encryptedPassword: (res as EncryptedPasswordObject)
+              .encryptedPassword,
+          };
+          sendMessageFromBackground(message);
+        });
+        break;
+      default:
+        break;
+    }
   }
-});
+);
