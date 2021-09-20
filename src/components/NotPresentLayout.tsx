1dce2f10fc2ced99664a2e623fbc8b515725a12c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-20 22:01:38 +0100

    updated not present layout

--- src/components/NotPresentLayout.tsx
+++ src/components/NotPresentLayout.tsx
@@ -1,3 +1,4 @@
+import { VStack, Text, Code, Box } from "@chakra-ui/react";
 import React from "react";
 import LoginDetailsDrawer from "./LoginDetailsDrawer";
 
@@ -11,13 +12,24 @@ const NotPresentLayout: React.FC<NotPresentLayoutProps> = ({
   setButtonCounter,
 }) => {
   return (
-    <div>
-      <LoginDetailsDrawer
-        UpdateOrSet="Set"
-        buttonCounter={buttonCounter}
-        setButtonCounter={setButtonCounter}
-      />
-    </div>
+    <VStack pt={4} spacing={4} alignItems="flex-start">
+      <Text fontSize={14}>
+        Hi, this is most likely the first time that you are using this
+        extension.
+      </Text>
+      <Text fontSize={14}>
+        Currently there are no login details present in localStorage (
+        <Code>chrome.storage.local</Code>). Please click the button below, to
+        add details.
+      </Text>
+      <Box alignSelf="center">
+        <LoginDetailsDrawer
+          UpdateOrSet="Set"
+          buttonCounter={buttonCounter}
+          setButtonCounter={setButtonCounter}
+        />
+      </Box>
+    </VStack>
   );
 };
 
