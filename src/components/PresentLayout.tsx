a11601d46e8d27da76d3763aadf7c6b14eea688c
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-09-21 17:13:03 +0100

    present layout

--- src/components/PresentLayout.tsx
+++ src/components/PresentLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RemoveDataButton from "./RemoveDataButton";
 import LoginDetailsDrawer from "./LoginDetailsDrawer";
+import { Code, Flex, Link, VStack, Text, Box } from "@chakra-ui/react";
 
 interface PresentLayoutProps {
   buttonCounter: number;
@@ -12,17 +13,33 @@ const PresentLayout: React.FC<PresentLayoutProps> = ({
   setButtonCounter,
 }) => {
   return (
-    <div>
-      <LoginDetailsDrawer
-        UpdateOrSet="Update"
-        buttonCounter={buttonCounter}
-        setButtonCounter={setButtonCounter}
-      />
-      <RemoveDataButton
-        buttonCounter={buttonCounter}
-        setButtonCounter={setButtonCounter}
-      />
-    </div>
+    <VStack spacing={8} pt={20} w="full" h="full">
+      <Flex w="full" h="full" direction="row" justifyContent="space-evenly">
+        <LoginDetailsDrawer
+          UpdateOrSet="Update"
+          buttonCounter={buttonCounter}
+          setButtonCounter={setButtonCounter}
+        />
+        <RemoveDataButton
+          buttonCounter={buttonCounter}
+          setButtonCounter={setButtonCounter}
+        />
+      </Flex>
+      <Box spacing="auto">
+        <Text fontSize={14}>
+          If you want to learn more about how this extension works and how it
+          uses your data please have a read of the, {""}
+          <Code>README</Code> on Github (
+          <a
+            href="https://github.com/LakeeSiv/cam-moodle-auto-login#readme"
+            target="_blank"
+          >
+            <Link>link</Link>
+          </a>
+          ).
+        </Text>
+      </Box>
+    </VStack>
   );
 };
 
