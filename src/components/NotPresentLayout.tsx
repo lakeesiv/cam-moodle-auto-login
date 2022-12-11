64b1740895a3372a171b9778b0b73e5b3a712bcd
Lakee Sivaraya <lakeesivaraya@gmail.com>
2022-12-11 13:31:13 +0000

    removed first time details

--- src/components/NotPresentLayout.tsx
+++ src/components/NotPresentLayout.tsx
@@ -14,10 +14,6 @@ const NotPresentLayout: React.FC<NotPresentLayoutProps> = ({
   return (
     <VStack pt={4} spacing={4} alignItems="flex-start">
       <Text fontSize={14}>
-        Hi, this is most likely the first time that you are using this
-        extension.
-      </Text>
-      <Text fontSize={14}>
         Currently there are no login details present in localStorage (
         <Code>chrome.storage.local</Code>). Please click the button below, to
         add details.
